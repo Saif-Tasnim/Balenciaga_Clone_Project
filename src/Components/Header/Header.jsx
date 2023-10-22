c1dd250ec1249fab45cc0db798c856cdf71c7571
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-10-22 19:03:35 +0600

    navbar updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,12 +6,56 @@ import bookmark from '../../assets/bookmark.png';
 const Header = () => {
 
     const link = <>
-        <Link className="navbar-item uppercase text-sm w-28 hover:border-[1px] hover:border-[#242424] hover:p-1 hover:rounded-md">New Arrival </Link>
-        <Link className="navbar-item uppercase text-sm hover:border-[1px] hover:border-[#242424] hover:p-2 hover:rounded-md"> Women </Link>
-        <Link className="navbar-item uppercase text-sm hover:border-[1px] hover:border-[#242424] hover:p-2 hover:rounded-md"> Men </Link>
-        <Link className="navbar-item uppercase text-sm hover:border-[1px] hover:border-[#242424] hover:p-2 hover:rounded-md"> Gifts </Link>
-        <Link className="navbar-item uppercase text-sm hover:border-[1px] hover:border-[#242424] hover:p-2 hover:rounded-md"> Couture </Link>
-        <Link className="navbar-item uppercase text-sm hover:border-[1px] hover:border-[#242424] hover:p-2 hover:rounded-md"> Explore </Link>
+        {/* new Arrivals */}
+
+        <div className="dropdown dropdown-hover">
+            <label className="navbar-item uppercase text-sm w-28 hover:border-[1px] hover:border-[#242424] hover:p-1 hover:rounded-md" tabIndex="0">New Arrival</label>
+            <div className="dropdown-menu dropdown-menu-bottom-right mt-5 rounded-md border-[1px] border-black flex flex-row w-[400px] h-[80px] justify-between items-center">
+                <div className='border-r-[1px] border-black h-full'>
+                    <Link className="dropdown-item text-sm hover:underline mr-8 mt-2"> New Arrivals - Women </Link>
+                </div>
+                <div>
+                    <Link tabIndex="-1" className="dropdown-item text-sm hover:underline mr-7"> New Arrivals - Men </Link>
+                </div>
+
+            </div>
+        </div>
+
+        {/* Women */}
+        <div className="dropdown dropdown-hover">
+            <label className="navbar-item uppercase text-sm w-28 hover:border-[1px] hover:border-[#242424] hover:p-1 hover:rounded-md" tabIndex="0">Women</label>
+            <div className="dropdown-menu dropdown-menu-bottom-right mt-5 rounded-md border-[1px] border-black">
+                <Link className="dropdown-item text-sm hover:underline"> Women </Link>
+                <Link tabIndex="-1" className="dropdown-item text-sm hover:underline"> Women Dress </Link>
+               
+            </div>
+        </div>
+
+        {/* Men */}
+        <div className="dropdown dropdown-hover">
+            <label className="navbar-item uppercase text-sm w-28 hover:border-[1px] hover:border-[#242424] hover:p-1 hover:rounded-md" tabIndex="0">Men</label>
+            <div className="dropdown-menu dropdown-menu-bottom-right mt-5 rounded-md border-[1px] border-black">
+                <Link className="dropdown-item text-sm hover:underline"> Men </Link>
+                <Link tabIndex="-1" className="dropdown-item text-sm hover:underline">Men Dress</Link>
+                
+            </div>
+        </div>
+
+        {/* gifts */}
+        <div className="dropdown dropdown-hover">
+            <label className="navbar-item uppercase text-sm w-28 hover:border-[1px] hover:border-[#242424] hover:p-1 hover:rounded-md" tabIndex="0">Gifts</label>
+            <div className="dropdown-menu dropdown-menu-bottom-right mt-5 rounded-md border-[1px] border-black">
+                <Link className="dropdown-item text-sm hover:underline">Gifts</Link>
+                <Link tabIndex="-1" className="dropdown-item text-sm hover:underline">Men</Link>
+                <Link tabIndex="-1" className="dropdown-item text-sm hover:underline">Women</Link>
+            </div>
+        </div>
+
+        {/* Explore */}
+        <div className="dropdown dropdown-hover">
+            <label className="navbar-item uppercase text-sm w-28 hover:border-[1px] hover:border-[#242424] hover:p-1 hover:rounded-md" tabIndex="0">Explore</label>
+            
+        </div>
     </>
 
     const lastLink = <>
